a42eb9b176631d32aa35aac6c1d32c62939f7098
Christoph Hermann <christoph.hermann@itbh.at>
2021-12-19 22:31:43 +0100

    Add test with buffers

--- src/test/java/at/itbh/LineParserBufferTest.java
+++ src/test/java/at/itbh/LineParserBufferTest.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import javax.inject.Inject;
 import org.junit.jupiter.api.Test;
 import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.helpers.test.AssertSubscriber;
 import io.vertx.mutiny.core.Vertx;
 import io.vertx.mutiny.core.buffer.Buffer;
@@ -15,16 +16,28 @@ public class LineParserBufferTest {
 
     @Inject
     Vertx vertx;
- 
+
     @Test
     public void testMultiLineStringWithEmptyLinesCRLF() {
         var parser = new LineParser(vertx, Charset.defaultCharset());
-        var inputs = new String[]{"", "This is one line", "", "", "", "This is another line", "", "This is a third line", "", ""};
+        var inputs = new String[] {"", "This is one line", "", "", "", "This is another line", "",
+                "This is a third line", "", ""};
         var buffer = Buffer.buffer(String.join("\r\n", inputs), Charset.defaultCharset().name());
         var assertSubscriber =
                 parser.parse(buffer).subscribe().withSubscriber(AssertSubscriber.create(3));
-                assertSubscriber.assertCompleted();
-                assertSubscriber.assertItems(new String[]{inputs[1], inputs[5], inputs[7]});
+        assertSubscriber.assertCompleted();
+        assertSubscriber.assertItems(new String[] {inputs[1], inputs[5], inputs[7]});
+    }
+
+    @Test
+    public void testMultiLineStringInBuffers() {
+        var parser = new LineParser(vertx, Charset.defaultCharset());
+        var buffers = new Buffer[] {Buffer.buffer("line one\n"), Buffer.buffer("line two\r\n"),
+                Buffer.buffer(""), Buffer.buffer("line three\n"), Buffer.buffer("line four\n")};
+        var assertSubscriber = parser.parse(Multi.createFrom().items(buffers)).log().subscribe()
+                .withSubscriber(AssertSubscriber.create(4));
+        assertSubscriber.assertCompleted().assertItems("line one", "line two", "line three",
+                "line four");
     }
 
 }
