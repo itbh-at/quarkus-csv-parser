88205143a23c6dc8da610548437ca1b919229eb0
Christoph Hermann <christoph.hermann@itbh.at>
2021-12-20 10:59:15 +0100

    Fix parsing logic error

--- src/test/java/at/itbh/LineParserFileTest.java
+++ src/test/java/at/itbh/LineParserFileTest.java
@@ -1,8 +1,14 @@
 package at.itbh;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.charset.Charset;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import javax.inject.Inject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import io.quarkus.test.junit.QuarkusTest;
 import io.smallrye.mutiny.helpers.test.AssertSubscriber;
@@ -15,12 +21,24 @@ public class LineParserFileTest {
     Vertx vertx;
 
     @Test
-    public void testFile() {
-        var parser = new LineParser(vertx, Charset.forName("utf8"));
+    public void testFile() throws FileNotFoundException {
+        var parser = new LineParser(vertx, Charset.forName("utf8"), 1);
         var fileParser = parser.parse(new File("src/test/resources/LineParser_test_file.txt"));
-        fileParser.log().onCompletion().invoke(() -> System.out.println("completed")).onItem()
-                .invoke(line -> System.out.println(line)).subscribe()
-                .withSubscriber(AssertSubscriber.create(3)).assertCompleted();
+        fileParser.subscribe().withSubscriber(AssertSubscriber.create(3)).awaitCompletion()
+                .assertCompleted()
+                .assertItems(new String[] {"First line", "Second line", "Third line"});
+    }
+
+    @Test
+    public void testFileBig() throws FileNotFoundException, InterruptedException {
+        var parser = new LineParser(vertx, Charset.forName("utf8"));
+        var fileParser = parser.parse(new File("src/test/resources/bigfile.txt"));
+        AtomicInteger counter = new AtomicInteger(0);
+        Instant startTimestamp = Instant.now();
+        fileParser.subscribe()
+        .asIterable().forEach(line -> counter.incrementAndGet());
+        Assertions.assertEquals(true, Duration.between(startTimestamp, Instant.now()).get(ChronoUnit.SECONDS) < 20);
+        Assertions.assertEquals(1000000, counter.get());
     }
 
 }
