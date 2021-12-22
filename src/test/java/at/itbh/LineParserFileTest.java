652517f2cd28816876bc865d112bde61b720f1f4
Christoph Hermann <christoph.hermann@itbh.at>
2021-12-22 13:37:41 +0100

    Use zipped big file instead of plain text
    The plain text file exceeds GitHubs max. file size

--- src/test/java/at/itbh/LineParserFileTest.java
+++ src/test/java/at/itbh/LineParserFileTest.java
@@ -1,14 +1,21 @@
 package at.itbh;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import javax.inject.Inject;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import io.quarkus.arc.log.LoggerName;
 import io.quarkus.test.junit.QuarkusTest;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.helpers.test.AssertSubscriber;
@@ -19,6 +26,63 @@ public class LineParserFileTest {
 
     @Inject
     Vertx vertx;
+
+    @BeforeAll
+    static void setup() throws IOException {
+        File bigFile = new File("src/test/resources/bigfile.txt");
+        if (bigFile.exists()) {
+            System.out.println("Deleting big file");
+            bigFile.delete();
+        }
+
+        // from https://www.baeldung.com/java-compress-and-uncompress
+        File destDir = new File("src/test/resources/");
+        byte[] buffer = new byte[4096];
+        ZipInputStream zis = new ZipInputStream(
+                new FileInputStream(new File("src/test/resources/bigfile.txt.zip")));
+        ZipEntry zipEntry = zis.getNextEntry();
+        System.out.println("Unzipping big file");
+        while (zipEntry != null) {
+            System.out.println("Extracting content of " + zipEntry.getName());
+            File newFile = newFile(destDir, zipEntry);
+            if (zipEntry.isDirectory()) {
+                if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                    throw new IOException("Failed to create directory " + newFile);
+                }
+            } else {
+                // fix for Windows-created archives
+                File parent = newFile.getParentFile();
+                if (!parent.isDirectory() && !parent.mkdirs()) {
+                    throw new IOException("Failed to create directory " + parent);
+                }
+
+                // write file content
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+            }
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+        System.out.println("Test setup complete");
+    }
+
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
+    }
 
     /**
      * Parses a tiny file in standard parsing mode
@@ -42,9 +106,11 @@ public class LineParserFileTest {
      */
     @Test
     public void testFileBig() throws InterruptedException, IOException {
-        var fileParser = buildFileParser(new File("src/test/resources/bigfile.txt"), LineParser.Mode.NIO);
+        var fileParser =
+                buildFileParser(new File("src/test/resources/bigfile.txt"), LineParser.Mode.NIO);
         runTestFileBig(fileParser, LineParser.Mode.NIO);
-        fileParser = buildFileParser(new File("src/test/resources/bigfile.txt"), LineParser.Mode.ASYNC_FILE);
+        fileParser = buildFileParser(new File("src/test/resources/bigfile.txt"),
+                LineParser.Mode.ASYNC_FILE);
         runTestFileBig(fileParser, LineParser.Mode.ASYNC_FILE);
     }
 
@@ -53,7 +119,8 @@ public class LineParserFileTest {
         int[] counter = {0};
         Instant startTimestamp = Instant.now();
         fileParser.subscribe().asIterable().forEach(line -> counter[0]++);
-        System.out.println(mode + ": big file test runtime: " + (System.currentTimeMillis() - startTime) + "ms");
+        System.out.println(mode + ": big file test runtime: "
+                + (System.currentTimeMillis() - startTime) + "ms");
         Assertions.assertEquals(true,
                 Duration.between(startTimestamp, Instant.now()).get(ChronoUnit.SECONDS) < 40);
         Assertions.assertEquals(10000000, counter[0]);
